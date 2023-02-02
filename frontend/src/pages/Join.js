4c9dd00d5c75a31431bba9662632fcbd37796b16
bhlee <bhlee@wezlesoft.co.kr>
2023-02-02 18:00:22 +0900

    백업

--- frontend/src/pages/Join.js
+++ frontend/src/pages/Join.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, {useEffect, useState} from "react";
 import { useNavigate } from "react-router-dom";
 
 const Join = () => {
@@ -9,7 +9,12 @@ const Join = () => {
   });
   const inputStyle = {
     width: 'calc(100% - 60px)',
+    marginBottom:0
   }
+  const [check, setCheck] = useState({
+    idChk: "",
+    pwChk: "",
+  });
 
   // e.target.name = input의 name(id,password)
   const onChangeAccount = (e) => {
@@ -18,6 +23,24 @@ const Join = () => {
       [e.target.name]: e.target.value,
     });
   };
+
+  const login = () => {
+    navigate("/");
+  };
+
+  const doubleCheck = () => {
+    if (account.id.length >= 5) {
+      setCheck({
+        ...check,
+        idChk: "중복된 아이디가 없습니다"
+      });
+    } else {
+      setCheck({
+        ...check,
+        idChk: "아이디가 중복됩니다"
+      });
+    }
+  }
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -36,7 +59,8 @@ const Join = () => {
           onChange={onChangeAccount}
           placeholder="id"
         />
-        <button className="chk-btn">중복확인</button>
+        <button type="button" className="chk-btn" onClick={doubleCheck}>중복확인</button>
+        <span id="id-chk" name="idChk"></span>
 
         <div>Password</div>
         <input
@@ -46,8 +70,18 @@ const Join = () => {
           onChange={onChangeAccount}
           placeholder="password"
         />
-        <spna>sadfdsf</spna>
-        <input  type="submit" value="Login" />
+        <span id="pw-chk" name="pwChk"></span>
+        <div>Name</div>
+        <input
+            type="text"
+            name="name"
+            onChange={onChangeAccount}
+            placeholder="name"
+        />
+        <input type="submit" value="회원가입" />
+        <div className="login-box">
+          <span onClick={login}>로그인</span><span> | </span><span>아이디 찾기</span><span> | </span><span>비밀번호 찾기</span>
+        </div>
       </form>
     </div>
   );
